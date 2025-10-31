d9fa191812b98d2a562e7670fe7ed0e6e4a0e3d6
joshua-odubu <freelance.odubu@gmail.com>
2025-10-31 13:43:24 +0000

    Fix App.jsx imports for Header, MainContent, and Footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-// src/App.jsx
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
     <>
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   );
 }
